ac0db746d2f5a8331dcc583caf82a87903fac7d1
Sean Morgan <insideseanshead@gmail.com>
2021-09-21 23:02:26 -0700

    created app.js

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+require('dotenv').config();
+// Sets up express app
+// ============================================================
+var app = express();
+var PORT = process.env.PORT || 8000;
+const cors = requre('cors')
+var allRoutes = require('./controllers');
+
+// Requiring modesl for syncing
+var db = require('./models');
+
+// Sets up express app  to handle data parsing
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+//Production CORS
+// app.use(cors({
+//     origin: [""]
+// }))
+
+
+// DEV CORS
+app.use(cors());
+
+app.use('/', allRoutes);
+
+db.sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function(){
+        console.log(`App listenting on PORT ${PORT}`);
+    });
+});
